7562fc713ef94a840579149a5206d51e80ed45f1
michael.couck <michael.couck@89a0fb20-889e-aefa-90f6-7c296b20d5cf>
2010-11-24 16:00:48 +0000

    git-svn-id: http://ikube.googlecode.com/svn/trunk@95 89a0fb20-889e-aefa-90f6-7c296b20d5cf

---
+++ modules/core/src/main/java/ikube/database/Index.java
@@ -0,0 +1,26 @@
+package ikube.database;
+
+import java.util.List;
+
+public class Index {
+
+	private String className;
+	private List<String> fieldNames;
+
+	public String getClassName() {
+		return className;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+	public List<String> getFieldNames() {
+		return fieldNames;
+	}
+
+	public void setFieldNames(List<String> fieldNames) {
+		this.fieldNames = fieldNames;
+	}
+
+}
