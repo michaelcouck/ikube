0fb1b428f64428fbfd920bb92dd73035f926fb27
michael.couck <michael.couck@89a0fb20-889e-aefa-90f6-7c296b20d5cf>
2010-12-16 09:46:21 +0000

    git-svn-id: http://ikube.googlecode.com/svn/trunk@182 89a0fb20-889e-aefa-90f6-7c296b20d5cf

--- modules/core/src/test/java/ikube/IntegrationTest.java
+++ modules/core/src/test/java/ikube/IntegrationTest.java
@@ -1,18 +1,34 @@
 package ikube;
 
-import ikube.toolkit.ApplicationContextManager;
+import java.io.File;
+import java.util.Map;
 
+import ikube.model.IndexContext;
+import ikube.toolkit.ApplicationContextManager;
+import ikube.toolkit.FileUtilities;
+
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class IntegrationTest extends ATest {
+	
+	@Before
+	public void before() {
+		// Delete all the old index directories
+		Map<String, IndexContext> contexts = ApplicationContextManager.getBeans(IndexContext.class);
+		for (IndexContext indexContext : contexts.values()) {
+			File baseIndexDirectory = FileUtilities.getFile(indexContext.getIndexDirectoryPath(), Boolean.TRUE);
+			FileUtilities.deleteFile(baseIndexDirectory, 1);
+		}
+	}
 
 	@Test
-	@Ignore
+	// @Ignore
 	public void index() throws Exception {
 		ApplicationContextManager.getApplicationContext(IConstants.SPRING_CONFIGURATION_FILE);
 		// And we sleep until the index is created
-		Thread.sleep(1000 * 60 * 60 * 3);
+		Thread.sleep(1000 * 60 * 60 * 100);
 	}
 
 }
