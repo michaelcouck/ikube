48f7c2ab39b00cbb28b663cf9489ec8efd5abbc7
michael.couck <michael.couck@89a0fb20-889e-aefa-90f6-7c296b20d5cf>
2011-02-07 20:12:21 +0000

    Release one.
    git-svn-id: http://ikube.googlecode.com/svn/trunk@303 89a0fb20-889e-aefa-90f6-7c296b20d5cf

---
+++ release.bat
@@ -0,0 +1,9 @@
+set MAVEN_OPTS=-Xms512m -Xmx1024m -XX:MaxPermSize=256m
+
+rem To have a dry run: -DdryRun=true
+rem To rollback the changes: release:rollback
+rem No interation: svn --non-interactive
+rem Want to resume: -Dresume=false 
+rem And the passwords: -Dusername=%1 -Dpassword=%2 
+
+mvn -DXms512m -DXmx1024m -DXX:MaxPermSize=256m release:clean release:prepare release:perform
