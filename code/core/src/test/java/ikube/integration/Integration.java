e3d867ea11bf4b5b86e597fe2afd735aff9e4da8
michael.couck <michael.couck@89a0fb20-889e-aefa-90f6-7c296b20d5cf>
2011-03-15 19:07:38 +0000

    Updating the integration configuration.
    git-svn-id: http://ikube.googlecode.com/svn/trunk@356 89a0fb20-889e-aefa-90f6-7c296b20d5cf

--- code/core/src/test/java/ikube/integration/Integration.java
+++ code/core/src/test/java/ikube/integration/Integration.java
@@ -1,7 +1,15 @@
 package ikube.integration;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+import ikube.IConstants;
 import ikube.logging.Logging;
+import ikube.model.faq.Attachment;
+import ikube.model.faq.Faq;
 import ikube.toolkit.ApplicationContextManager;
+import ikube.toolkit.datageneration.DataGeneratorFour;
+import ikube.toolkit.datageneration.IDataGenerator;
 
 import org.apache.log4j.Logger;
 import org.junit.Test;
@@ -18,7 +26,7 @@ public class Integration {
 	static {
 		Logging.configure();
 	}
-	
+
 	private Logger logger = Logger.getLogger(this.getClass());
 
 	@Test
@@ -26,10 +34,19 @@ public class Integration {
 		String osName = System.getProperty("os.name");
 		logger.info("Operating system : " + osName);
 		if (!osName.toLowerCase().contains("server")) {
-			return;
+			// return;
 		}
 		ApplicationContextManager.getApplicationContext();
+		generateData();
 		Thread.sleep(1000 * 60 * 60 * 3);
+	}
+
+	private void generateData() throws Exception {
+		EntityManager entityManager = Persistence.createEntityManagerFactory(IConstants.PERSISTENCE_UNIT_NAME).createEntityManager();
+		IDataGenerator dataGenerator = new DataGeneratorFour(entityManager, 100, Faq.class, Attachment.class);
+		dataGenerator.before();
+		dataGenerator.generate();
+		dataGenerator.after();
 	}
 
 }
