499b12b2f5b8d01fda75bc16a47bcdb33198594e
michael.couck <michael.couck@89a0fb20-889e-aefa-90f6-7c296b20d5cf>
2011-04-17 16:48:15 +0000

    Updated the start cluster script and added a restore test.
    git-svn-id: http://ikube.googlecode.com/svn/trunk@411 89a0fb20-889e-aefa-90f6-7c296b20d5cf

--- code/core/src/main/java/ikube/action/Restore.java
+++ code/core/src/main/java/ikube/action/Restore.java
@@ -1,9 +1,14 @@
 package ikube.action;
 
+import ikube.IConstants;
+import ikube.index.IndexManager;
 import ikube.model.IndexContext;
+import ikube.toolkit.FileUtilities;
+
+import java.io.File;
 
 /**
- * TODO Implement me. This class restores an index that has been back up to the index directory designated for indexes.
+ * This class restores an index that has been back up to the index directory designated for indexes.
  * 
  * @author Michael Couck
  * @since 08.04.11
@@ -15,8 +20,16 @@ public class Restore extends Action<IndexContext, Boolean> {
 	public Boolean execute(final IndexContext indexContext) {
 		try {
 			getClusterManager().setWorking(indexContext.getIndexName(), this.getClass().getName(), Boolean.TRUE);
-			// See if there is a problems with the index, i.e. if it is corrupt
-			// If corrupt or the index contexts are not initialized copy the index to the designated directory for indexes
+			// Get the latest backup index
+			// Copy the backup to the index directory
+			// Change the name to the max age + 1 hour
+			File latestIndexDirectoryBackup = FileUtilities.getLatestIndexDirectory(indexContext.getIndexDirectoryPathBackup());
+			String indexDirectoryPath = IndexManager.getIndexDirectoryPathBackup(indexContext);
+			long time = System.currentTimeMillis() + (1000 * 60 * 60);
+			String restoredIndexDirectoryPath = indexDirectoryPath + IConstants.SEP + time;
+			File restoredIndexDirectory = FileUtilities.getFile(restoredIndexDirectoryPath, Boolean.TRUE);
+			logger.info("Restoring index from : " + latestIndexDirectoryBackup + ", to : " + restoredIndexDirectory);
+			FileUtilities.copyFiles(latestIndexDirectoryBackup, restoredIndexDirectory);
 		} finally {
 			getClusterManager().setWorking(indexContext.getIndexName(), this.getClass().getName(), Boolean.FALSE);
 		}
---
+++ code/core/src/test/java/ikube/action/RestoreTest.java
@@ -0,0 +1,82 @@
+package ikube.action;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+import ikube.ATest;
+import ikube.mock.ApplicationContextManagerMock;
+import ikube.toolkit.FileUtilities;
+
+import java.io.File;
+
+import mockit.Mockit;
+
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests the restore process that copies an index from the backup directory to the index directory and sets the time stamp ahead of the max
+ * age to give the server a time to recover before starting a new index.
+ * 
+ * @author Michael Couck
+ * @since 17.04.11
+ * @version 01.00
+ */
+public class RestoreTest extends ATest {
+
+	private Restore restore;
+
+	public RestoreTest() {
+		super(RestoreTest.class);
+	}
+
+	@Before
+	public void before() throws Exception {
+		restore = new Restore();
+		FileUtilities.deleteFile(new File(INDEX_CONTEXT.getIndexDirectoryPath()), 1);
+		FileUtilities.deleteFile(new File(INDEX_CONTEXT.getIndexDirectoryPathBackup()), 1);
+	}
+
+	@After
+	public void after() throws Exception {
+		Mockit.tearDownMocks();
+		FileUtilities.deleteFile(new File(INDEX_CONTEXT.getIndexDirectoryPath()), 1);
+		FileUtilities.deleteFile(new File(INDEX_CONTEXT.getIndexDirectoryPathBackup()), 1);
+	}
+
+	@Test
+	public void execute() throws Exception {
+		// Create an index in the normal directory
+		File latestIndexDirectory = createIndex(INDEX_CONTEXT, "the original text fragment");
+
+		// Create an index in the backup directory
+		when(INDEX_CONTEXT.getIndexDirectoryPath()).thenReturn(indexDirectoryPathBackup);
+		createIndex(INDEX_CONTEXT, "a little text");
+		when(INDEX_CONTEXT.getIndexDirectoryPath()).thenReturn(indexDirectoryPath);
+
+		// Corrupt the index
+		FileUtilities.deleteFiles(latestIndexDirectory, "segments");
+
+		// Run the restore
+		Mockit.setUpMocks(ApplicationContextManagerMock.class);
+		restore.execute(INDEX_CONTEXT);
+		Mockit.tearDownMocks();
+
+		// Check that the index is restored
+		latestIndexDirectory = FileUtilities.getLatestIndexDirectory(INDEX_CONTEXT.getIndexDirectoryPath());
+		File latestServerIndexDirectory = new File(latestIndexDirectory, IP);
+		Directory directory = null;
+		try {
+			directory = FSDirectory.open(latestServerIndexDirectory);
+			assertTrue(IndexReader.indexExists(directory));
+		} finally {
+			if (directory != null) {
+				directory.close();
+			}
+		}
+	}
+
+}
