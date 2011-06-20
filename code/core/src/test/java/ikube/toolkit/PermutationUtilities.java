c6fe27481a04ea9acbf78d1ac1d988688f4fdcba
michael.couck@gmail.com <michael.couck@gmail.com@89a0fb20-889e-aefa-90f6-7c296b20d5cf>
2011-06-20 13:52:31 +0000

    Added a JavaDoc to a method.
    git-svn-id: http://ikube.googlecode.com/svn/trunk@482 89a0fb20-889e-aefa-90f6-7c296b20d5cf

--- code/core/src/test/java/ikube/toolkit/PermutationUtilities.java
+++ code/core/src/test/java/ikube/toolkit/PermutationUtilities.java
@@ -27,6 +27,29 @@ public class PermutationUtilities {
 		getPermutations(newObjects, permutations, N, 0);
 	}
 
+  /**
+     * This method will get permutations for a certain array of objects. For example if there is an
+     * array of three objects, new Object[] {one, two, three}, then all the possible permutations
+     * for the array are:
+     * 
+     * <pre>
+     *  1) [two, three, one]
+     *  2) [three, two, one]
+     *  3) [three, one, two]
+     *  4) [one, three, two]
+     *  5) [two, one, three]
+     *  6) [one, two, three]
+     * </pre>
+     * 
+     * Note that the number of permutations increases exponentially with the increase in the number
+     * of objects.
+     * 
+     * @param <T> t the type
+     * @param objects the objects to get the permutation for
+     * @param permutations some permutations
+     * @param numberOfObjects some value
+     * @param counter the counter you idiot
+     */
 	public <T> void getPermutations(T[] objects, List<T[]> permutations, int n, int counter) {
 		if (n == 1) {
 			permutations.add(Arrays.copyOf(objects, objects.length));
