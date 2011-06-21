8d46656302dd009149e07cbe2bcf756d6a50ea48
michael.couck@gmail.com <michael.couck@gmail.com@89a0fb20-889e-aefa-90f6-7c296b20d5cf>
2011-06-21 14:12:49 +0000

    Made most of the fields transient.
    git-svn-id: http://ikube.googlecode.com/svn/trunk@484 89a0fb20-889e-aefa-90f6-7c296b20d5cf

--- code/core/src/main/java/ikube/model/Url.java
+++ code/core/src/main/java/ikube/model/Url.java
@@ -5,6 +5,7 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.Transient;
 
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
@@ -31,14 +32,18 @@ public class Url extends Persistable {
 	public static final String SELECT_FROM_URL_WHERE_ID_GREATER_AND_NOT_INDEXED = "select u from Url as u where u.id >= :id and u.indexed = :indexed";
 
 	private long urlId;
-	private String url;
 	private boolean indexed;
-	/** The hash of the content. */
 	private long hash;
 
-	private String contentType;
+	@Transient
+  private String url;
+  @Transient
+  private String contentType;
+  @Transient
 	private String title;
+  @Transient
 	private byte[] rawContent;
+  @Transient
 	private String parsedContent;
 
 	public long getUrlId() {
