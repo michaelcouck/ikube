4d86f07ed6b9691eb50cbc7bd82f1cf6f9f4f086
michael.couck <michael.couck@89a0fb20-889e-aefa-90f6-7c296b20d5cf>
2011-07-08 07:42:21 +0000

    Removed the integration test.
    git-svn-id: http://ikube.googlecode.com/svn/trunk@496 89a0fb20-889e-aefa-90f6-7c296b20d5cf

--- code/war/src/test/java/ikube/integration/Integration.java
+++ code/war/src/test/java/ikube/integration/Integration.java
@@ -72,7 +72,7 @@ public class Integration {
 		if (!osName.toLowerCase().contains("server") && properties.getProperty("os.arch").contains("64")) {
 			return Boolean.FALSE;
 		}
-		return Boolean.TRUE;
+		return Boolean.FALSE;
 	}
 
 }
