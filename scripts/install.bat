e7b64f50381a24a73fcc22d2c0e6db731c73896d
michael.couck <michael.couck@89a0fb20-889e-aefa-90f6-7c296b20d5cf>
2011-07-15 06:16:55 +0000

    Updated the configuration.
    git-svn-id: http://ikube.googlecode.com/svn/trunk@505 89a0fb20-889e-aefa-90f6-7c296b20d5cf

--- scripts/install.bat
+++ scripts/install.bat
@@ -2,4 +2,4 @@ cd /d ../
 set MAVEN_OPTS=-Xms1024m -Xmx2048m -XX:PermSize=128m -XX:MaxPermSize=256m
 rem -DskipTests=true javadoc:javadoc
 rem -DskipTests=true -DskipITs=true
-mvn clean install
+mvn clean install -DskipTests=true -DskipITs=true
