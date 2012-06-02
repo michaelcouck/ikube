62c45c6e3820f9af25cc89f7f896b74e844c86b2
michael.couck@gmail.com <michael.couck@gmail.com@89a0fb20-889e-aefa-90f6-7c296b20d5cf>
2012-06-02 06:20:29 +0000

    Init the parser mappings.
    git-svn-id: http://ikube.googlecode.com/svn/trunk@908 89a0fb20-889e-aefa-90f6-7c296b20d5cf

--- code/war/src/test/java/ikube/web/Integration.java
+++ code/war/src/test/java/ikube/web/Integration.java
@@ -1,26 +1,29 @@
 package ikube.web;
 
 import ikube.IConstants;
+import ikube.index.parse.mime.MimeMapper;
+import ikube.index.parse.mime.MimeTypes;
 import ikube.security.WebServiceAuthentication;
-import ikube.toolkit.SerializationUtilities;
+import ikube.toolkit.Logging;
 import ikube.toolkit.ThreadUtilities;
-import ikube.web.service.Searcher;
 
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.NameValuePair;
-import org.apache.commons.httpclient.methods.GetMethod;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Ignore
 public abstract class Integration {
+
+	static {
+		Logging.configure();
+		new MimeTypes(IConstants.MIME_TYPES);
+		new MimeMapper(IConstants.MIME_MAPPING);
+		ThreadUtilities.initialize();
+	}
 
 	protected static String LOCALHOST = "localhost";
 	/** This client({@link HttpClient}) is for the web services. */
@@ -28,8 +31,6 @@ public abstract class Integration {
 	protected static int SERVER_PORT = 9080;
 	protected static String REST_USER_NAME = "user";
 	protected static String REST_PASSWORD = "user";
-
-	private static final Logger LOGGER = LoggerFactory.getLogger(Integration.class);
 
 	/**
 	 * Authentication for the web client.
@@ -41,33 +42,6 @@ public abstract class Integration {
 	public static void beforeClass() throws Exception {
 		// We need to wait for the geospatial index to be created
 		WebServiceAuthentication.authenticate(HTTP_CLIENT, LOCALHOST, SERVER_PORT, REST_USER_NAME, REST_PASSWORD);
-		// We'll wait here for the integration indexes to be ready
-		while (indexesInitialized()) {
-			LOGGER.info("Waiting : ");
-			// Thread.sleep(60000);
-		}
-		ThreadUtilities.initialize();
-	}
-
-	@SuppressWarnings("unchecked")
-	private static final boolean indexesInitialized() throws Exception {
-		// String, String, String, boolean, int, int
-		String path = IConstants.SEP + IConstants.IKUBE + Searcher.SERVICE + Searcher.SEARCH + Searcher.SINGLE;
-		String url = new URL("http", LOCALHOST, SERVER_PORT, path).toString();
-
-		String[] names = { IConstants.INDEX_NAME, IConstants.SEARCH_STRINGS, IConstants.SEARCH_FIELDS, IConstants.FRAGMENT,
-				IConstants.FIRST_RESULT, IConstants.MAX_RESULTS };
-		String[] values = { IConstants.GEOSPATIAL, "cape AND town AND university", IConstants.NAME, Boolean.TRUE.toString(), "0", "10" };
-		NameValuePair[] params = getNameValuePairs(names, values);
-
-		GetMethod getMethod = new GetMethod(url);
-		getMethod.setQueryString(params);
-		int result = HTTP_CLIENT.executeMethod(getMethod);
-		String actual = getMethod.getResponseBodyAsString();
-		List<Map<String, String>> resultsList = (List<Map<String, String>>) SerializationUtilities.deserialize(actual);
-		LOGGER.info("Result : " + result + ", " + resultsList.size());
-
-		return resultsList.size() > 1;
 	}
 
 	protected static NameValuePair[] getNameValuePairs(String[] names, String[] values) {
