7ea0d5b82c38638fa0eb9c49e575caa999c0bfa3
michael.couck <michael.couck@89a0fb20-889e-aefa-90f6-7c296b20d5cf>
2013-06-19 16:33:44 +0000

    Added some code for the Twitter connector using Spring social.
    git-svn-id: http://ikube.googlecode.com/svn/trunk@1432 89a0fb20-889e-aefa-90f6-7c296b20d5cf

--- code/core/src/main/java/ikube/action/index/handler/internet/TwitterHandler.java
+++ code/core/src/main/java/ikube/action/index/handler/internet/TwitterHandler.java
@@ -1,21 +1,30 @@
 package ikube.action.index.handler.internet;
 
+import ikube.action.index.handler.IResourceProvider;
 import ikube.action.index.handler.IndexableHandler;
 import ikube.model.IndexContext;
 import ikube.model.Indexable;
 import ikube.model.IndexableInternet;
-import ikube.security.WebServiceAuthentication;
+import ikube.toolkit.ThreadUtilities;
 
-import java.io.InputStream;
-import java.net.URL;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.Future;
+import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.atomic.AtomicInteger;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpMethod;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.io.IOUtils;
+import org.springframework.social.twitter.api.Stream;
+import org.springframework.social.twitter.api.StreamDeleteEvent;
+import org.springframework.social.twitter.api.StreamListener;
+import org.springframework.social.twitter.api.StreamWarningEvent;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.social.twitter.api.UserStreamParameters;
+import org.springframework.social.twitter.api.UserStreamParameters.WithOptions;
+import org.springframework.social.twitter.api.impl.TwitterTemplate;
 
 /**
  * @author Michael Couck
@@ -24,38 +33,72 @@ import org.apache.commons.io.IOUtils;
  */
 public class TwitterHandler extends IndexableHandler<IndexableInternet> {
 
+	class ResourceProvider implements IResourceProvider<Tweet> {
+
+		private Stack<Tweet> tweets = new Stack<>();
+
+		ResourceProvider(final IndexableInternet indexableInternet) throws IOException {
+			TwitterTemplate twitter = new TwitterTemplate("YR571S2JiVBOFyJS5MEg", "Kb8hS0luftwCJX3qVoyiLUMfZDtK1EozFoUkjNLUMx4",
+					"7078572-srXzIDwmIc0lg69TBR0rLr9TqHYJiRqPIv14gunpk", "xfnQDjYEcdTCcYWeLcJyGuszQG9R5UnG5TrvtfG33BU");
+			List<StreamListener> listeners = new ArrayList<StreamListener>();
+			StreamListener listener = new StreamListener() {
+				public void onTweet(Tweet tweet) {
+					logger.info("TWEET:  " + tweet.getFromUser() + "   -   " + tweet.getText());
+					tweets.push(tweet);
+				}
+
+				public void onLimit(int numberOfLimitedTweets) {
+					logger.info("LIMIT:  " + numberOfLimitedTweets);
+				}
+
+				public void onDelete(StreamDeleteEvent deleteEvent) {
+					logger.info("DELETE:  " + deleteEvent.getTweetId());
+				}
+
+				public void onWarning(StreamWarningEvent warnEvent) {
+					logger.info("WARNING:  " + warnEvent.getCode());
+				}
+			};
+			listeners.add(listener);
+			UserStreamParameters params = new UserStreamParameters().with(WithOptions.FOLLOWINGS).includeReplies(true);
+			// Stream userStream = twitter.streamingOperations().sample(listeners);
+			Stream userStream = twitter.streamingOperations().filter("Disney", listeners);
+			// twitter.streamingOperations().sample(listeners);
+			ThreadUtilities.sleep(100000);
+			userStream.stop();
+		}
+
+		public Tweet getResource() {
+			if (tweets.isEmpty()) {
+				return null;
+			}
+			return tweets.pop();
+		}
+
+		@Override
+		public void setResources(List<Tweet> resources) {
+			tweets.addAll(resources);
+		}
+
+	}
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public List<Future<?>> handleIndexable(final IndexContext<?> indexContext, final IndexableInternet indexableInternet) throws Exception {
-		// The start url
-		List<Future<?>> futures = new ArrayList<Future<?>>();
-		HttpClient httpClient = new HttpClient();
-		login(indexableInternet, httpClient);
-		HttpMethod httpMethod = new GetMethod(indexableInternet.getBaseUrl());
-		int response = httpClient.executeMethod(httpMethod);
-		InputStream inputStream = httpMethod.getResponseBodyAsStream();
-		List<String> lines = IOUtils.readLines(inputStream);
-		logger.info("Response : " + response + ", " + lines);
-		return futures;
+		final AtomicInteger threads = new AtomicInteger(indexableInternet.getThreads());
+		ForkJoinPool forkJoinPool = new ForkJoinPool(threads.get());
+		ResourceProvider fileResourceProvider = new ResourceProvider(indexableInternet);
+		RecursiveAction recursiveAction = getRecursiveAction(indexContext, indexableInternet, fileResourceProvider);
+		forkJoinPool.invoke(recursiveAction);
+		return new ArrayList<Future<?>>(Arrays.asList(recursiveAction));
 	}
-	
+
 	@Override
 	protected List<?> handleResource(final IndexContext<?> indexContext, final Indexable<?> indexable, final Object resource) {
 		logger.info("Handling resource : " + resource + ", thread : " + Thread.currentThread().hashCode());
 		return null;
 	}
 
-	void login(final IndexableInternet indexableInternet, final HttpClient httpClient) {
-		URL loginUrl;
-		try {
-			loginUrl = new URL(indexableInternet.getLoginUrl());
-			String userid = indexableInternet.getUserid();
-			String password = indexableInternet.getPassword();
-			new WebServiceAuthentication().authenticate(httpClient, loginUrl.getHost(), "80", userid, password);
-		} catch (Exception e) {
-			logger.error("Exception logging in to site : " + indexableInternet, e);
-		}
-	}
 }
--- code/core/src/test/java/ikube/action/index/handler/internet/TwitterHandlerTest.java
+++ code/core/src/test/java/ikube/action/index/handler/internet/TwitterHandlerTest.java
@@ -3,17 +3,28 @@ package ikube.action.index.handler.internet;
 import static ikube.toolkit.ObjectToolkit.populateFields;
 import ikube.AbstractTest;
 import ikube.model.IndexableInternet;
+import ikube.toolkit.ThreadUtilities;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Future;
 
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.springframework.social.twitter.api.Stream;
+import org.springframework.social.twitter.api.StreamDeleteEvent;
+import org.springframework.social.twitter.api.StreamListener;
+import org.springframework.social.twitter.api.StreamWarningEvent;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.social.twitter.api.impl.TwitterTemplate;
 
 /**
  * @author Michael Couck
  * @since 21.11.10
  * @version 01.00
  */
-@Ignore
+// @Ignore
 public class TwitterHandlerTest extends AbstractTest {
 
 	private TwitterHandler twitterHandler;
@@ -24,13 +35,50 @@ public class TwitterHandlerTest extends AbstractTest {
 	}
 
 	@Test
+	@Ignore
 	public void handleIndexable() throws Exception {
+		ThreadUtilities.initialize();
 		IndexableInternet indexableInternet = populateFields(IndexableInternet.class, new IndexableInternet(), Boolean.TRUE, 5);
-		indexableInternet.setBaseUrl("http://twitter.com/statuses/friends_timeline.xml");
-		indexableInternet.setLoginUrl("http://twitter.com/statuses/friends_timeline.xml");
-		indexableInternet.setUserid("michaelcouck");
-		indexableInternet.setPassword("caherline");
-		twitterHandler.handleIndexable(indexContext, indexableInternet);
+		indexableInternet.setThreads(1);
+		List<Future<?>> futures = twitterHandler.handleIndexable(indexContext, indexableInternet);
+		ThreadUtilities.waitForFutures(futures, 60000);
+	}
+
+	@Test
+	public void twitterApi() {
+		TwitterTemplate twitter = new TwitterTemplate("YR571S2JiVBOFyJS5MEg", "Kb8hS0luftwCJX3qVoyiLUMfZDtK1EozFoUkjNLUMx4",
+				"7078572-srXzIDwmIc0lg69TBR0rLr9TqHYJiRqPIv14gunpk", "xfnQDjYEcdTCcYWeLcJyGuszQG9R5UnG5TrvtfG33BU");
+		StreamListener listener = new StreamListener() {
+
+			@Override
+			public void onTweet(Tweet tweet) {
+				logger.info("TWEET:  " + tweet.getFromUser() + "   -   " + tweet.getText());
+			}
+
+			@Override
+			public void onLimit(int numberOfLimitedTweets) {
+				logger.info("LIMIT:  " + numberOfLimitedTweets);
+			}
+
+			@Override
+			public void onDelete(StreamDeleteEvent deleteEvent) {
+				logger.info("DELETE:  " + deleteEvent.getTweetId());
+			}
+
+			@Override
+			public void onWarning(StreamWarningEvent warnEvent) {
+				logger.info("WARNING:  " + warnEvent.getCode());
+			}
+
+		};
+		// UserStreamParameters params = new UserStreamParameters().with(WithOptions.FOLLOWINGS).includeReplies(true);
+		// Stream userStream = twitter.streamingOperations().sample(listeners);
+		// Stream userStream = twitter.streamingOperations().filter("Disney", listeners);
+		// Stream userStream = twitter.streamingOperations().firehose(Arrays.asList(listener));
+		// Stream userStream = twitter.streamingOperations().sample(listeners);
+		Stream userStream = twitter.streamingOperations().sample(Arrays.asList(listener));
+		ThreadUtilities.sleep(100000);
+		userStream.stop();
 	}
 
 }
