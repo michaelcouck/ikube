b93bffae2b9134f611d5e8fb0ff1b882f430b7fe
michael.couck <michael.couck@89a0fb20-889e-aefa-90f6-7c296b20d5cf>
2013-09-23 15:30:52 +0000

    Bla...
    git-svn-id: http://ikube.googlecode.com/svn/trunk@1613 89a0fb20-889e-aefa-90f6-7c296b20d5cf

--- code/core/src/test/java/ikube/analytics/WekaTest.java
+++ code/core/src/test/java/ikube/analytics/WekaTest.java
@@ -8,11 +8,13 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import weka.classifiers.Classifier;
 import weka.classifiers.functions.SMO;
 import weka.core.Attribute;
+import weka.core.FastVector;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.SparseInstance;
@@ -20,18 +22,43 @@ import weka.core.SparseInstance;
 public class WekaTest extends AbstractTest {
 
 	@Test
+	@Ignore
 	public void arff() throws Exception {
 		File file = FileUtilities.findFileRecursively(new File("."), "instance.arff");
 		FileReader fileReader = new FileReader(file);
 		BufferedReader bufferedReader = new BufferedReader(fileReader);
 		Instances instances = new Instances(bufferedReader);
 		logger.info(instances.toString());
+
+		Instance instance = new Instance(1.0, new double[] { 1, 2, 3, 4 });
+
 		Classifier classifier = new SMO();
 		classifier.buildClassifier(instances);
-		
-		Instance instance = makeInstance("hello world", instances);
 	}
-	
+
+	@Test
+	public void build() throws Exception {
+		FastVector classAttInfo = new FastVector(2);
+		classAttInfo.addElement(IConstants.POSITIVE);
+		classAttInfo.addElement(IConstants.NEGATIVE);
+
+		FastVector attInfo = new FastVector(2);
+		attInfo.addElement(new Attribute("@@class@@", classAttInfo));
+		attInfo.addElement(new Attribute("@@vectors@@"));
+
+		Instances instances = new Instances("instances", attInfo, 100000);
+		instances.setClassIndex(0);
+
+		double[] attValues = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		int[] indices = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		SparseInstance sparseInstance = new SparseInstance(1.0, attValues, indices, Integer.MAX_VALUE);
+
+		instances.add(sparseInstance);
+
+		Classifier classifier = new SMO();
+		classifier.buildClassifier(instances);
+	}
+
 	/**
 	 * TODO Document me...
 	 * 
@@ -39,7 +66,7 @@ public class WekaTest extends AbstractTest {
 	 * @param instances
 	 * @return
 	 */
-	private synchronized Instance makeInstance(final String text, final Instances instances) {
+	synchronized Instance makeInstance(final String text, final Instances instances) {
 		// Create instance of length two.
 		SparseInstance instance = new SparseInstance(2);
 		// Set value for message attribute
