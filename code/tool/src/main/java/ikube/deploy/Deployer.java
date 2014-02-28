7b5dc77bf56defa5b70e7755432d81d16d01d326
michael.couck@gmail.com <michael.couck@gmail.com@89a0fb20-889e-aefa-90f6-7c296b20d5cf>
2014-02-28 15:03:42 +0000

    Fixed some of the gui logic and the clustering and distribution of tasks into the cluster.
    git-svn-id: http://ikube.googlecode.com/svn/trunk@1934 89a0fb20-889e-aefa-90f6-7c296b20d5cf

--- code/tool/src/main/java/ikube/deploy/Deployer.java
+++ code/tool/src/main/java/ikube/deploy/Deployer.java
@@ -5,46 +5,43 @@ import ikube.deploy.model.Server;
 import ikube.toolkit.FileUtilities;
 import ikube.toolkit.Logging;
 import ikube.toolkit.ThreadUtilities;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.concurrent.Future;
-
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.concurrent.Future;
+
 /**
  * @author Michael Couck
- * @since 18-06-13
  * @version 01.00
+ * @since 18-06-2013
  */
 public final class Deployer {
 
-	private static final Logger LOGGER;
+    private static final Logger LOGGER;
 
-	static {
-		Logging.configure();
-		LOGGER = LoggerFactory.getLogger(Deployer.class);
-	}
-
-	private static final String DOT_DIRECTORY = ".";
-	private static final String CONFIGURATION_FILE = "deployer\\.xml";
-
-	private static ApplicationContext APPLICATION_CONTEXT;
-
-	public static void main(final String[] args) {
+    static {
+        Logging.configure();
         ThreadUtilities.initialize();
-		File configurationDirectory = new File(DOT_DIRECTORY);
-		String configurationFile = CONFIGURATION_FILE;
-		boolean execute = Boolean.TRUE;
-		if (args != null) {
-			if (args.length > 1) {
+        LOGGER = LoggerFactory.getLogger(Deployer.class);
+    }
+
+    private static final String DOT_DIRECTORY = ".";
+    private static final String CONFIGURATION_FILE = "deployer\\.xml";
+
+    private static ApplicationContext APPLICATION_CONTEXT;
+
+    public static void main(final String[] args) {
+        File configurationDirectory = new File(DOT_DIRECTORY);
+        String configurationFile = CONFIGURATION_FILE;
+        boolean execute = Boolean.TRUE;
+        if (args != null) {
+            if (args.length > 1) {
                 int upDirectories = 0;
                 while (args[0].contains("../")) {
                     args[0] = args[0].replace("../", "");
@@ -54,78 +51,84 @@ public final class Deployer {
                     args[0] = ".";
                 }
                 LOGGER.info("Args : " + upDirectories + ", " + Arrays.deepToString(args));
-				configurationDirectory = new File(args[0]);
+                configurationDirectory = new File(args[0]);
                 LOGGER.info("Conf dir before : " + configurationDirectory);
                 configurationDirectory = FileUtilities.moveUpDirectories(configurationDirectory, upDirectories);
                 LOGGER.info("Conf dir after moving : " + configurationDirectory);
                 configurationFile = args[1];
-			}
-			if (args.length >= 3) {
-				execute = Boolean.valueOf(args[2]);
-			}
-		}
-		String configurationDirectoryPath = FileUtilities.cleanFilePath(configurationDirectory.getAbsolutePath());
-		LOGGER.info("Directory : " + configurationDirectoryPath + ", file : " + configurationFile);
-		List<Future<?>> futures = new ArrayList<>();
-		// Find the configuration file
-		File deployerConfiguration = FileUtilities.findFileRecursively(new File(configurationDirectoryPath), configurationFile);
-		LOGGER.info("Configuration file : " + deployerConfiguration);
-		String deployerConfigurationPath = "file:" + FileUtilities.cleanFilePath(deployerConfiguration.getAbsolutePath());
-		LOGGER.info("Configuration file path : " + deployerConfigurationPath);
-		APPLICATION_CONTEXT = new FileSystemXmlApplicationContext(deployerConfigurationPath);
-		if (execute) {
-			Deployer deployer = APPLICATION_CONTEXT.getBean(Deployer.class);
-			for (final Server server : deployer.getServers()) {
+            }
+            if (args.length >= 3) {
+                execute = Boolean.valueOf(args[2]);
+            }
+        }
+        String configurationDirectoryPath = FileUtilities.cleanFilePath(configurationDirectory.getAbsolutePath());
+        LOGGER.info("Directory : " + configurationDirectoryPath + ", file : " + configurationFile);
+        // Find the configuration file
+        File deployerConfiguration = FileUtilities.findFileRecursively(new File(configurationDirectoryPath), configurationFile);
+        LOGGER.info("Configuration file : " + deployerConfiguration);
+        String deployerConfigurationPath = "file:" + FileUtilities.cleanFilePath(deployerConfiguration.getAbsolutePath());
+        LOGGER.info("Configuration file path : " + deployerConfigurationPath);
+        APPLICATION_CONTEXT = new FileSystemXmlApplicationContext(deployerConfigurationPath);
+        if (execute) {
+            Deployer deployer = APPLICATION_CONTEXT.getBean(Deployer.class);
+            for (final Server server : deployer.getServers()) {
                 String name = Long.toString(System.currentTimeMillis());
-				Future<?> future = ThreadUtilities.submit(name, new Runnable() {
-					public void run() {
-						for (final IAction action : server.getActions()) {
-							action.execute(server);
-						}
-					}
-				});
-				futures.add(future);
-				if (!deployer.isParallel()) {
-					ThreadUtilities.waitForFuture(future, deployer.getMaxWaitTime());
-				}
-			}
-			ThreadUtilities.waitForFutures(futures, deployer.getMaxWaitTime());
-		}
-	}
+                if (deployer.isParallel()) {
+                    Future<?> future = ThreadUtilities.submit(name, new Runnable() {
+                        public void run() {
+                            for (final IAction action : server.getActions()) {
+                                action.execute(server);
+                            }
+                        }
+                    });
+                    ThreadUtilities.waitForFuture(future, deployer.getMaxWaitTime());
+                } else {
+                    for (final IAction action : server.getActions()) {
+                        Future<?> future = ThreadUtilities.submit(name, new Runnable() {
+                            public void run() {
+                                action.execute(server);
+                            }
+                        });
+                        ThreadUtilities.waitForFuture(future, deployer.getMaxWaitTime());
+                    }
+                }
+            }
+        }
+    }
 
-	public static ApplicationContext getApplicationContext() {
-		if (APPLICATION_CONTEXT == null) {
-			main(new String[] { DOT_DIRECTORY, CONFIGURATION_FILE, "false" });
-		}
-		return APPLICATION_CONTEXT;
-	}
+    public static ApplicationContext getApplicationContext() {
+        if (APPLICATION_CONTEXT == null) {
+            main(new String[]{DOT_DIRECTORY, CONFIGURATION_FILE, "false"});
+        }
+        return APPLICATION_CONTEXT;
+    }
 
-	private boolean parallel;
-	private long maxWaitTime = 600000;
-	private Collection<Server> servers;
+    private boolean parallel;
+    private long maxWaitTime = 60;
+    private Collection<Server> servers;
 
-	public boolean isParallel() {
-		return parallel;
-	}
+    public boolean isParallel() {
+        return parallel;
+    }
 
-	public void setParallel(boolean parallel) {
-		this.parallel = parallel;
-	}
+    public void setParallel(boolean parallel) {
+        this.parallel = parallel;
+    }
 
-	public long getMaxWaitTime() {
-		return maxWaitTime;
-	}
+    public long getMaxWaitTime() {
+        return maxWaitTime;
+    }
 
-	public void setMaxWaitTime(long maxWaitTime) {
-		this.maxWaitTime = maxWaitTime;
-	}
+    public void setMaxWaitTime(long maxWaitTime) {
+        this.maxWaitTime = maxWaitTime;
+    }
 
-	public Collection<Server> getServers() {
-		return servers;
-	}
+    public Collection<Server> getServers() {
+        return servers;
+    }
 
-	public void setServers(Collection<Server> servers) {
-		this.servers = servers;
-	}
+    public void setServers(Collection<Server> servers) {
+        this.servers = servers;
+    }
 
 }
