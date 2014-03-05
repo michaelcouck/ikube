a2b30e72128d033db271610f00b7d09a61ae37ac
michael.couck@gmail.com <michael.couck@gmail.com@89a0fb20-889e-aefa-90f6-7c296b20d5cf>
2014-03-05 16:16:22 +0000

    Bug fixing and system testing.
    git-svn-id: http://ikube.googlecode.com/svn/trunk@1961 89a0fb20-889e-aefa-90f6-7c296b20d5cf

--- code/core/src/main/java/ikube/scheduling/schedule/IndexCommitSchedule.java
+++ code/core/src/main/java/ikube/scheduling/schedule/IndexCommitSchedule.java
@@ -3,54 +3,54 @@ package ikube.scheduling.schedule;
 import ikube.cluster.IMonitorService;
 import ikube.model.IndexContext;
 import ikube.scheduling.Schedule;
+import org.apache.lucene.index.IndexWriter;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
 import java.util.Map;
 
-import org.apache.lucene.index.IndexWriter;
-import org.springframework.beans.factory.annotation.Autowired;
-
 /**
- * This schedule will periodically commit the indexes that are currently being written. This is desirable in configurations where the indexes are large and are
- * written over NFS. The indexes will be merged as well. Lucene threading will take care of the underlying details of concurrent access to the index files.
- * 
+ * This schedule will periodically commit the indexes that are currently being written. This is desirable
+ * in configurations where the indexes are large and are written over NFS. The indexes will be merged as well.
+ * Lucene threading will take care of the underlying details of concurrent access to the index files.
+ *
  * @author Michael Couck
- * @since 21.06.13
  * @version 01.00
+ * @since 21-06-2013
  */
 public class IndexCommitSchedule extends Schedule {
 
-	@Autowired
-	private IMonitorService monitorService;
+    @Autowired
+    private IMonitorService monitorService;
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	@SuppressWarnings({ "rawtypes" })
-	public void run() {
-		Map<String, IndexContext> indexContexts = monitorService.getIndexContexts();
-		for (Map.Entry<String, IndexContext> mapEntry : indexContexts.entrySet()) {
-			IndexContext<?> indexContext = mapEntry.getValue();
-			if (!indexContext.isDelta()) {
-				continue;
-			}
-			IndexWriter[] indexWriters = indexContext.getIndexWriters();
-			if (indexWriters == null || indexWriters.length == 0) {
-				continue;
-			}
-			for (final IndexWriter indexWriter : indexWriters) {
-				try {
-					logger.info("Comitting index : " + indexContext.getName());
-					indexWriter.maybeMerge();
-					indexWriter.forceMerge(10, Boolean.FALSE);
-					indexWriter.commit();
-					indexWriter.deleteUnusedFiles();
-				} catch (IOException e) {
-					logger.error("Exception comitting the index writer : ", e);
-				}
-			}
-		}
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @SuppressWarnings({"rawtypes"})
+    public void run() {
+        Map<String, IndexContext> indexContexts = monitorService.getIndexContexts();
+        for (Map.Entry<String, IndexContext> mapEntry : indexContexts.entrySet()) {
+            IndexContext<?> indexContext = mapEntry.getValue();
+            if (!indexContext.isDelta()) {
+                continue;
+            }
+            IndexWriter[] indexWriters = indexContext.getIndexWriters();
+            if (indexWriters == null || indexWriters.length == 0) {
+                continue;
+            }
+            for (final IndexWriter indexWriter : indexWriters) {
+                try {
+                    logger.info("Comitting index : " + indexContext.getName());
+                    indexWriter.maybeMerge();
+                    indexWriter.forceMerge(10, Boolean.FALSE);
+                    indexWriter.commit();
+                    indexWriter.deleteUnusedFiles();
+                } catch (final IOException e) {
+                    logger.error("Exception comitting the index writer : ", e);
+                }
+            }
+        }
+    }
 
 }
