cae597c388ce64cfc42bb1bab4d93cb7cb9f1013
michael.couck@gmail.com <michael.couck@gmail.com@89a0fb20-889e-aefa-90f6-7c296b20d5cf>
2014-03-05 15:21:24 +0000

    Bug fixing and system testing.
    git-svn-id: http://ikube.googlecode.com/svn/trunk@1959 89a0fb20-889e-aefa-90f6-7c296b20d5cf

--- code/tool/src/main/java/ikube/deploy/action/CmdAction.java
+++ code/tool/src/main/java/ikube/deploy/action/CmdAction.java
@@ -2,6 +2,7 @@ package ikube.deploy.action;
 
 import ikube.deploy.model.Server;
 import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.common.IOUtils;
 import net.schmizz.sshj.connection.channel.direct.Session;
 import org.apache.commons.lang.StringUtils;
 
@@ -44,7 +45,7 @@ public class CmdAction extends Action {
 
                         Session session = sshExec.startSession();
                         Session.Command sessionCommand = session.exec(command);
-                        // sessionCommand.join(10, TimeUnit.SECONDS);
+                        sessionCommand.join(10, TimeUnit.SECONDS);
 
                         Integer exitStatus = sessionCommand.getExitStatus();
                         String errorMessage = sessionCommand.getExitErrorMessage();
@@ -55,9 +56,9 @@ public class CmdAction extends Action {
                             // for example, and the script terminates when the shell exits, i.e. it doesn't start the
                             // tomcat. We only read from the output if there is an error otherwise the process
                             // started, perhaps by a shell script, terminates *****immediately***** !!!!!
-                            /*String message = IOUtils.readFully(sessionCommand.getInputStream()).toString();
-                            String error = IOUtils.readFully(sessionCommand.getErrorStream()).toString();*/
-                            Object[] parameters = {errorMessage, exitStatus};
+                            String message = IOUtils.readFully(sessionCommand.getInputStream()).toString();
+                            String error = IOUtils.readFully(sessionCommand.getErrorStream()).toString();
+                            Object[] parameters = {message, errorMessage, error, exitStatus};
                             logger.info("Message : {}, error message : {}, error : {}, exit status : {}", parameters);
                         } else if (StringUtils.isNotEmpty(errorMessage)) {
                             logger.info("Error message : " + errorMessage);
