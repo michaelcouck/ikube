7f47206bcd93337c069b81509c17a4067e1c1edb
michael.couck@gmail.com <michael.couck@gmail.com@89a0fb20-889e-aefa-90f6-7c296b20d5cf>
2014-03-17 07:07:00 +0000

    Bug fixing and system testing.
    git-svn-id: http://ikube.googlecode.com/svn/trunk@2041 89a0fb20-889e-aefa-90f6-7c296b20d5cf

--- code/core/src/main/java/ikube/action/index/handler/internet/TwitterHandler.java
+++ code/core/src/main/java/ikube/action/index/handler/internet/TwitterHandler.java
@@ -4,60 +4,68 @@ import ikube.action.index.handler.IResourceProvider;
 import ikube.action.index.handler.IndexableHandler;
 import ikube.model.IndexContext;
 import ikube.model.IndexableTweets;
-
-import java.util.List;
-import java.util.concurrent.ForkJoinTask;
-
 import org.apache.lucene.document.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.Tweet;
 
+import java.util.List;
+import java.util.concurrent.ForkJoinTask;
+
 /**
- * This class is the configurable part of the Twitter source of data. It combines the resource provider {@link TwitterResourceProvider} and the
- * {@link TwitterResourceHandler} to get around 1% of the tweets from Twitter. Generally strategies are chained to the processing to calculate the sentiment of
- * the tweets. This additional data is used to enrich the index, which can then be used to generate time line reports on the twitter data.
- * 
+ * This class is the configurable part of the Twitter source of data. It combines the resource provider
+ * {@link TwitterResourceProvider} and the {@link TwitterResourceHandler} to get around 1% of the tweets
+ * from Twitter. Generally strategies are chained to the processing to calculate the sentiment of the tweets.
+ * This additional data is used to enrich the index, which can then be used to generate time line reports
+ * on the twitter data.
+ *
  * @author Michael Couck
- * @since 24.04.13
  * @version 01.00
+ * @since 24-04-2013
  */
 public class TwitterHandler extends IndexableHandler<IndexableTweets> {
 
-	@Autowired
-	private TwitterResourceHandler twitterResourceHandler;
+    @Autowired
+    private TwitterResourceHandler twitterResourceHandler;
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public ForkJoinTask<?> handleIndexableForked(final IndexContext<?> indexContext, final IndexableTweets indexableTweets) throws Exception {
-		IResourceProvider<Tweet> twitterResourceProvider = new TwitterResourceProvider(indexableTweets);
-		return getRecursiveAction(indexContext, indexableTweets, twitterResourceProvider);
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ForkJoinTask<?> handleIndexableForked(
+            final IndexContext<?> indexContext,
+            final IndexableTweets indexableTweets)
+            throws Exception {
+        IResourceProvider<Tweet> twitterResourceProvider = new TwitterResourceProvider(indexableTweets);
+        return getRecursiveAction(indexContext, indexableTweets, twitterResourceProvider);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	protected List<?> handleResource(final IndexContext<?> indexContext, final IndexableTweets indexableTweets, final Object resource) {
-		Tweet tweet = (Tweet) resource;
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @SuppressWarnings("StringBufferReplaceableByString")
+    protected List<?> handleResource(
+            final IndexContext<?> indexContext,
+            final IndexableTweets indexableTweets,
+            final Object resource) {
+        Tweet tweet = (Tweet) resource;
 
-		// Build the primary field in the index
-		StringBuilder builder = new StringBuilder();
-		builder.append(tweet.getFromUserId());
-		builder.append(" ");
-		builder.append(tweet.getFromUser());
-		builder.append(" ");
-		builder.append(tweet.getText());
+        // Build the primary field in the index
+        StringBuilder builder = new StringBuilder();
+        builder.append(tweet.getFromUserId());
+        builder.append(" ");
+        builder.append(tweet.getFromUser());
+        builder.append(" ");
+        builder.append(tweet.getText());
 
-		indexableTweets.setContent(builder.toString());
-		// And put the data in the index
-		try {
-			twitterResourceHandler.handleResource(indexContext, indexableTweets, new Document(), resource);
-		} catch (Exception e) {
-			handleException(indexableTweets, e, "Exception handling Twitter resource : " + tweet);
-		}
-		return null;
-	}
+        indexableTweets.setContent(builder.toString());
+        // And put the data in the index
+        try {
+            twitterResourceHandler.handleResource(indexContext, indexableTweets, new Document(), resource);
+        } catch (final Exception e) {
+            handleException(indexableTweets, e, "Exception handling Twitter resource : " + tweet);
+        }
+        return null;
+    }
 
 }
