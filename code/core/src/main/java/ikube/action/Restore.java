45b1b5a841f75d040f27a4542fedde3be2a5c91e
michael.couck@gmail.com <michael.couck@gmail.com@89a0fb20-889e-aefa-90f6-7c296b20d5cf>
2014-03-21 15:50:51 +0000

    Bug fixing and system testing.
    git-svn-id: http://ikube.googlecode.com/svn/trunk@2077 89a0fb20-889e-aefa-90f6-7c296b20d5cf

--- code/core/src/main/java/ikube/action/Restore.java
+++ code/core/src/main/java/ikube/action/Restore.java
@@ -3,50 +3,50 @@ package ikube.action;
 import ikube.IConstants;
 import ikube.action.index.IndexManager;
 import ikube.model.IndexContext;
-import ikube.toolkit.FileUtilities;
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
 
-import org.apache.commons.io.FileUtils;
-
 /**
  * This class restores an index that has been back up to the index directory designated for indexes.
- * 
+ *
  * @author Michael Couck
- * @since 08.04.11
  * @version 01.00
+ * @since 08-04-2011
  */
 public class Restore extends Action<IndexContext, Boolean> {
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	boolean internalExecute(final IndexContext indexContext) {
-		// Check that the index directory and the backup directory are not the same
-		if (indexContext.getIndexDirectoryPath().equals(indexContext.getIndexDirectoryPathBackup())) {
-			logger.debug("Index and backup paths are the same : ");
-			return Boolean.FALSE;
-		}
-		File latestIndexDirectoryBackup = null;
-		File restoredIndexDirectory = null;
-		// Get the latest backup index
-		String indexDirectoryPathBackup = IndexManager.getIndexDirectoryPathBackup(indexContext);
-		latestIndexDirectoryBackup = IndexManager.getLatestIndexDirectory(indexDirectoryPathBackup);
-		String indexDirectoryPath = IndexManager.getIndexDirectoryPath(indexContext);
-		// Change the name to the max age + 24 hours which will give enough time to run another index
-		long time = System.currentTimeMillis() + (1000 * 60 * 60 * 24);
-		String restoredIndexDirectoryPath = indexDirectoryPath + IConstants.SEP + time;
-		restoredIndexDirectory = FileUtilities.getFile(restoredIndexDirectoryPath, Boolean.TRUE);
-		logger.info("Restoring index from : " + latestIndexDirectoryBackup + ", to : " + restoredIndexDirectory);
-		try {
-			// Copy the backup to the index directory
-			FileUtils.copyDirectory(latestIndexDirectoryBackup, restoredIndexDirectory);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-		return Boolean.TRUE;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    boolean internalExecute(final IndexContext indexContext) {
+        // Check that the index directory and the backup directory are not the same
+        if (indexContext.getIndexDirectoryPath().equals(indexContext.getIndexDirectoryPathBackup())) {
+            logger.debug("Index and backup paths are the same : ");
+            return Boolean.FALSE;
+        }
+        File latestIndexDirectoryBackup;
+        File restoredIndexDirectory;
+        // Get the latest backup index
+        String indexDirectoryPathBackup = IndexManager.getIndexDirectoryPathBackup(indexContext);
+        latestIndexDirectoryBackup = IndexManager.getLatestIndexDirectory(indexDirectoryPathBackup);
+        String indexDirectoryPath = IndexManager.getIndexDirectoryPath(indexContext);
+        // Change the name to the max age + 24 hours which will give enough time to run another index
+        long time = System.currentTimeMillis() + (1000 * 60 * 60 * 24);
+        String restoredIndexDirectoryPath = indexDirectoryPath + IConstants.SEP + time;
+        restoredIndexDirectory = new File(restoredIndexDirectoryPath);
+        logger.info("Restoring index from : " + latestIndexDirectoryBackup + ", to : " + restoredIndexDirectory);
+        try {
+            // Move the backup to the hot index, tne backup action will back it up again if necessary
+            FileUtils.moveDirectory(latestIndexDirectoryBackup, restoredIndexDirectory);
+            // Try to back it up immediately any how
+            FileUtils.copyDirectory(restoredIndexDirectory, latestIndexDirectoryBackup);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return Boolean.TRUE;
+    }
 
 }
