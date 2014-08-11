3f57d70408f37d3bde831cc5cd82d7019b2229ac
davidturley <Erlang10>
2014-08-11 22:39:50 +0200

    add exchange client

--- code/core/src/main/java/ikube/model/IndexableExchange.java
+++ code/core/src/main/java/ikube/model/IndexableExchange.java
@@ -7,6 +7,8 @@ import javax.persistence.InheritanceType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+import java.util.Date;
+import ikube.action.index.handler.internet.exchange.IndexableMessage;
 
 /**
  * @author David Turley
@@ -39,6 +41,22 @@ public class IndexableExchange extends Indexable {
     @Attribute(field = false, description = "This is the password to login to the server")
     private String password;
 
+    private IndexableMessage indexableMessage;
+
+    @Column
+    @Size(min = 4, max = 60)
+    @Attribute(field = false, description = "Resume indexing messages from this user account email onward. Null indicates start from the beginning.")
+    private String resumeIndexFrom;
+
+    @Column
+    @Attribute(field = false, description = "Resume indexing messages from this user account email message onward. " +
+            "The message is an unique identifier of last message processed successfully. Null indicates start from the beginning.")
+    private String  resumeIndexFromMessage;
+
+    @Column
+    @Attribute(field = false, description = "This is the date to start indexing all user email messages from (message send date). Null indicates start from the beginning of time.")
+    private Date   indexFromDate;
+
     public String getUrl() {
         return url;
     }
@@ -62,4 +80,24 @@ public class IndexableExchange extends Indexable {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public IndexableMessage getIndexableMessage() {
+        return indexableMessage;
+    }
+
+    public void setIndexableMessage(IndexableMessage indexableMessage) {
+        this.indexableMessage = indexableMessage;
+    }
+
+    public Date   getIndexFromDate(){ return indexFromDate; }
+
+    public String getResumeIndexFrom(){ return resumeIndexFrom; }
+
+    public String getResumeIndexFromMessage(){ return resumeIndexFromMessage; }
+
+    public void setIndexFromDate(Date indexFromDate){ this.indexFromDate = indexFromDate; }
+
+    public void setResumeIndexFrom(String emailAddress){ this.resumeIndexFrom = emailAddress; }
+
+    public void setResumeIndexFromMessage(String emailMessageId){ this.resumeIndexFromMessage = emailMessageId; }
 }
